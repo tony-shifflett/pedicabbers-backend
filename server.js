8212b9720bee06d67023ce52093c64bf88ab7df9
Tony Shifflett <tonyshifflett@tonys-mbp.lan>
2021-05-18 23:03:01 -0500

    created configs directory and cors.js

---
+++ server.js
@@ -0,0 +1,45 @@
+///////////////////////////
+// Environmental Variables
+///////////////////////////
+require("dotenv").config();
+const { PORT = 3000, NODE_ENV = "development" } = process.env;
+
+//MONGO CONNECTION
+const mongoose = require("./db/connection");
+
+//CORS
+const cors = require("cors");
+const corsOptions = require("./configs/cors.js");
+
+//Bringing in Express
+const express = require("express");
+const app = express();
+
+//OTHER IMPORTS
+const morgan = require("morgan");
+const pedicabberRouter = require("./controllers/pedicabber");
+
+////////////
+//MIDDLEWARE
+////////////
+// NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny")); //logging
+
+///////////////
+//Routes and Routers
+//////////////
+
+//Route for testing server is working
+app.get("/", (req, res) => {
+  res.json({ hello: "Hello World!" });
+});
+
+// Dog Routes send to dog router
+app.use("/pedicabber", pedicabberRouter);
+
+//LISTENER
+app.listen(PORT, () => {
+  console.log(`Your are listening on port ${PORT}`);
+});
